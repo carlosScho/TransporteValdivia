c79cef8c268c8ddda9c4a50491c31bbeb9120133
Tomas vera jackson <t.vera.j@gmail.com>
2016-12-20 03:25:20 -0300

    arreglando error

--- bd_main.sql
+++ bd_main.sql
@@ -4,15 +4,26 @@ Create table Recorrido(	id_Recorrido int Primary key auto_increment,
 						Tiempo_Aprox time
 						);
 
-Create table Recorrido_Calle(RefRecorrido int Primary key,
-							 RefCalle int Primary key,
-							 FOREIGN key (RefRecorrido) REFERENCES Recorrido(id_Recorrido),
-							 FOREIGN key (RefCalle) REFERENCES Calle(id_Calle)
-							 );
-
 Create table Calle(	id_Calle int Primary key auto_increment, 
 					Nombre varchar(50) not null
 					);
+
+Create table Tarifa(id_Tarifa int Primary key auto_increment, 
+					Descripcion varchar(100), 
+					Monto int not null, 
+					);
+
+Create table Conductor(	Rut varchar(10) Primary key, 
+						Nombre varchar(30) not null, 
+						Apellido varchar(30) not null, 
+						Fecha_Nac date
+						);
+
+Create table Recorrido_Calle(RefRecorrido int not null,
+							 RefCalle int not null,
+							 FOREIGN key (RefRecorrido) REFERENCES Recorrido(id_Recorrido),
+							 FOREIGN key (RefCalle) REFERENCES Calle(id_Calle)
+							 );
 
 Create table Linea(	id_Linea int Primary key auto_increment,
 					Numero int not null,
@@ -25,22 +36,13 @@ Create table Linea(	id_Linea int Primary key auto_increment,
 					FOREIGN KEY (RefRecorrido) REFERENCES Recorrido(id_Recorrido)
 					);
 
-Create table Tarifa(id_Tarifa int Primary key auto_increment, 
-					Descripcion varchar(100), 
-					Monto int not null, 
-					);
 
-Create table Linea_Tarifa(	RefLinea int Primary key, 
-							Reftarifa int Primary key,
+Create table Linea_Tarifa(	RefLinea int not null, 
+							Reftarifa int not null,
 							FOREIGN key (RefLinea) REFERENCES Linea(id_Linea),
 							FOREIGN key (Reftarifa) REFERENCES Tarifa(id_Tarifa)
 							);
 
-Create table Conductor(	Rut varchar(10) Primary key, 
-						Nombre varchar(30) not null, 
-						Apellido varchar(30) not null, 
-						Fecha_Nac date
-						);
 
 Create table Vehiculo(	Patente varchar(6) Primary key, 
 						Capacidad int, 
