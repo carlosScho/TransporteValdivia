36992ef02f6bb1abaf619007f873a6826177a69f
TomasVJ <t.vera.j@gmail.com>
2016-12-20 13:08:28 -0300

    Agregando lugares

---
+++ insert_lugar.sql
@@ -0,0 +1,18 @@
+
+insert into Lugar_Principal(Nombre, Descripcion) values ('Casino', '');
+
+insert into Lugar_Principal(Nombre, Descripcion) values ('Hospital', '');
+
+insert into Lugar_Principal(Nombre, Descripcion) values ('UACH Campus Teja', 'Universidad Austral de Chile Campus Isla Teja');
+
+insert into Lugar_Principal(Nombre, Descripcion) values ('UACH Campus Miraflores', 'Universidad Austral de Chile Campus Miraflores');
+
+insert into Lugar_Principal(Nombre, Descripcion) values ('USS', 'Univesidad San Sebastian');
+
+insert into Lugar_Principal(Nombre, Descripcion) values ('UST', 'Universidad Santo Tomás');
+
+insert into Lugar_Principal(Nombre, Descripcion) values ('Inacap', 'Instituto Profecional Inacap');
+
+insert into Lugar_Principal(Nombre, Descripcion) values ('Mall', 'Centro comercial de Valdivia');
+
+insert into Lugar_Principal(Nombre, Descripcion) values ('Lider', 'Supermercado Lider');
