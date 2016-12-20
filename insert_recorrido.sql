339eaf7fe5885c02648cfd2828f7cfb436b3ef9b
Tomas vera jackson <t.vera.j@gmail.com>
2016-12-20 03:36:21 -0300

    correccion errores

--- insert_recorrido.sql
+++ insert_recorrido.sql
@@ -1,46 +1,46 @@
 --Micro 1
-INSERT INTO Recorrido VALUES('Balmaceda','Ingeniero Raul Saez','01:20:00');
+INSERT INTO Recorrido(Inicio, Fin, Tiempo_Aprox) VALUES('Balmaceda','Ingeniero Raul Saez','01:20:00');
 
 
 --Micro 2 A
 
-INSERT INTO Recorrido VALUES('Pedro Aguirre Cerda','Arica','01:50:00');
+INSERT INTO Recorrido(Inicio, Fin, Tiempo_Aprox) VALUES('Pedro Aguirre Cerda','Arica','01:50:00');
 
 --Micro 2 B
 
-INSERT INTO Recorrido VALUES('Pedro Aguirre Cerda','Havdaerbeck','01:50:00');
+INSERT INTO Recorrido(Inicio, Fin, Tiempo_Aprox) VALUES('Pedro Aguirre Cerda','Havdaerbeck','01:50:00');
 
 
 --Micro 3
 
-INSERT into Recorrido VALUES('Ingeniero Raul Saez', 'Picarte', '01:10:00');
+INSERT into Recorrido(Inicio, Fin, Tiempo_Aprox) VALUES('Ingeniero Raul Saez', 'Picarte', '01:10:00');
 
 
 
-INSERT into Recorrido VALUES('Las Gaviotas', 'Los Laureles (UACH)', '00:50:00');
+INSERT into Recorrido(Inicio, Fin, Tiempo_Aprox) VALUES('Las Gaviotas', 'Los Laureles (UACH)', '00:50:00');
 
 --Micro 20
 
-Insert into Recorrido VALUES('Ingeniero Raul Saez', 'T-352 (Niebla)', '01:30:00');
+Insert into Recorrido(Inicio, Fin, Tiempo_Aprox) VALUES('Ingeniero Raul Saez', 'T-352 (Niebla)', '01:30:00');
 
 
 
-INSERT into Recorrido VALUES('Av. Rene Schneider', 'Arauco', '00:50:00');
+INSERT into Recorrido(Inicio, Fin, Tiempo_Aprox) VALUES('Av. Rene Schneider', 'Arauco', '00:50:00');
 
 
 
-INSERT into Recorrido VALUES('Av. Rene Schneider', 'Los Laureles (UACH)', '01:10:00');
+INSERT into Recorrido(Inicio, Fin, Tiempo_Aprox) VALUES('Av. Rene Schneider', 'Los Laureles (UACH)', '01:10:00');
 
 
 
-INSERT into Recorrido VALUES('Av. Rene Schneider', 'Los Laureles (UACH)', '01:10:00');
+INSERT into Recorrido(Inicio, Fin, Tiempo_Aprox) VALUES('Av. Rene Schneider', 'Los Laureles (UACH)', '01:10:00');
 
 
 
-INSERT INTO Recorrido VALUES('Balmaceda','Los Laureles (UACH)','00:45:00');
+INSERT INTO Recorrido(Inicio, Fin, Tiempo_Aprox) VALUES('Balmaceda','Los Laureles (UACH)','00:45:00');
 
 
-INSERT INTO Recorrido VALUES('Ingeniero Raul Saez', 'Los Robles)','00:40:00');
+INSERT INTO Recorrido(Inicio, Fin, Tiempo_Aprox) VALUES('Ingeniero Raul Saez', 'Los Robles','00:40:00');
 
 
 
