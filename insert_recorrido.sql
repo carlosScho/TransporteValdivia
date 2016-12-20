913cadd2fc7c490084d98177f43c16930e4ff3d2
Tomas vera jackson <t.vera.j@gmail.com>
2016-12-20 04:10:55 -0300

    arreglando vehiculos

--- insert_recorrido.sql
+++ insert_recorrido.sql
@@ -24,17 +24,10 @@ INSERT into Recorrido(Inicio, Fin, Tiempo_Aprox) VALUES('Las Gaviotas', 'Los Lau
 Insert into Recorrido(Inicio, Fin, Tiempo_Aprox) VALUES('Ingeniero Raul Saez', 'T-352 (Niebla)', '01:30:00');
 
 
-
 INSERT into Recorrido(Inicio, Fin, Tiempo_Aprox) VALUES('Av. Rene Schneider', 'Arauco', '00:50:00');
 
 
-
 INSERT into Recorrido(Inicio, Fin, Tiempo_Aprox) VALUES('Av. Rene Schneider', 'Los Laureles (UACH)', '01:10:00');
-
-
-
-INSERT into Recorrido(Inicio, Fin, Tiempo_Aprox) VALUES('Av. Rene Schneider', 'Los Laureles (UACH)', '01:10:00');
-
 
 
 INSERT INTO Recorrido(Inicio, Fin, Tiempo_Aprox) VALUES('Balmaceda','Los Laureles (UACH)','00:45:00');
