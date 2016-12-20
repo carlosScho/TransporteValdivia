640a37f02db661da4c1fac23171d112b0b8147f6
carlosScho <byas_crudos@hotmail.com>
2016-12-20 12:46:49 -0300

    actualizando tablas

--- bd_main.sql
+++ bd_main.sql
@@ -50,14 +50,15 @@ Create table Vehiculo(	Patente varchar(6) Primary key,
 						RefLinea int not null,
 						FOREIGN KEY (RefLinea) REFERENCES Linea(id_Linea),
 						FOREIGN KEY (RefConductor) REFERENCES Conductor(Rut)
-						);
+					 );
 
-Create table Lugar_Principal(	id_Lugar int Primary key,
+Create table Lugar_Principal(	id_Lugar int Primary key auto_increment,
 								Nombre varchar(50),
 								Descripcion varchar(100)
 							);
-Create table LPrincipal_Calle (	RefLugar int not null,
+
+Create table LPrincipal_Calle(	RefLugar int not null,
 								RefCalle int not null,
 								FOREIGN KEY (RefLugar) REFERENCES Lugar_Principal(id_Lugar),
 								FOREIGN KEY (RefCalle) REFERENCES Calle(id_Calle)
-							);
+							 );
