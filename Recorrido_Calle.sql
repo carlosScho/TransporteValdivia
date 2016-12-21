7f0f56232be75ee078262e28737b04757102db28
Carlos Schoenfeldt <carlos.schoenfeldt@alumnos.uach.cl>
2016-12-21 01:55:42 -0300

    recorridos de un colectivo y una micro
    hasta el momento, para probar

---
+++ Recorrido_Calle.sql
@@ -0,0 +1,56 @@
+--linea 45
+
+INSERT INTO Recorrido_Calle(refRecorrido,refCalle) VALUES (11,37);
+INSERT INTO Recorrido_Calle(refRecorrido,refCalle) VALUES (11,16);
+INSERT INTO Recorrido_Calle(refRecorrido,refCalle) VALUES (11,44);
+INSERT INTO Recorrido_Calle(refRecorrido,refCalle) VALUES (11,23);
+INSERT INTO Recorrido_Calle(refRecorrido,refCalle) VALUES (11,2);
+INSERT INTO Recorrido_Calle(refRecorrido,refCalle) VALUES (11,55);
+INSERT INTO Recorrido_Calle(refRecorrido,refCalle) VALUES (11,57);
+INSERT INTO Recorrido_Calle(refRecorrido,refCalle) VALUES (11,56);
+INSERT INTO Recorrido_Calle(refRecorrido,refCalle) VALUES (11,25);
+INSERT INTO Recorrido_Calle(refRecorrido,refCalle) VALUES (11,24);
+INSERT INTO Recorrido_Calle(refRecorrido,refCalle) VALUES (11,52);
+INSERT INTO Recorrido_Calle(refRecorrido,refCalle) VALUES (11,73);
+INSERT INTO Recorrido_Calle(refRecorrido,refCalle) VALUES (11,74);
+INSERT INTO Recorrido_Calle(refRecorrido,refCalle) VALUES (11,75);
+INSERT INTO Recorrido_Calle(refRecorrido,refCalle) VALUES (11,76);
+INSERT INTO Recorrido_Calle(refRecorrido,refCalle) VALUES (11,77);
+INSERT INTO Recorrido_Calle(refRecorrido,refCalle) VALUES (11,78);
+INSERT INTO Recorrido_Calle(refRecorrido,refCalle) VALUES (11,79);
+INSERT INTO Recorrido_Calle(refRecorrido,refCalle) VALUES (11,80);
+INSERT INTO Recorrido_Calle(refRecorrido,refCalle) VALUES (11,81);
+INSERT INTO Recorrido_Calle(refRecorrido,refCalle) VALUES (11,82);
+INSERT INTO Recorrido_Calle(refRecorrido,refCalle) VALUES (11,19);
+
+
+--micro 3
+
+INSERT INTO Recorrido_Calle(refRecorrido,refCalle) VALUES (4,1);
+INSERT INTO Recorrido_Calle(refRecorrido,refCalle) VALUES (4,2);
+INSERT INTO Recorrido_Calle(refRecorrido,refCalle) VALUES (4,3);
+INSERT INTO Recorrido_Calle(refRecorrido,refCalle) VALUES (4,4);
+INSERT INTO Recorrido_Calle(refRecorrido,refCalle) VALUES (4,5);
+INSERT INTO Recorrido_Calle(refRecorrido,refCalle) VALUES (4,6);
+INSERT INTO Recorrido_Calle(refRecorrido,refCalle) VALUES (4,7);
+INSERT INTO Recorrido_Calle(refRecorrido,refCalle) VALUES (4,8);
+INSERT INTO Recorrido_Calle(refRecorrido,refCalle) VALUES (4,83);
+INSERT INTO Recorrido_Calle(refRecorrido,refCalle) VALUES (4,10);
+INSERT INTO Recorrido_Calle(refRecorrido,refCalle) VALUES (4,14);
+INSERT INTO Recorrido_Calle(refRecorrido,refCalle) VALUES (4,15);
+INSERT INTO Recorrido_Calle(refRecorrido,refCalle) VALUES (4,16);
+INSERT INTO Recorrido_Calle(refRecorrido,refCalle) VALUES (4,17);
+INSERT INTO Recorrido_Calle(refRecorrido,refCalle) VALUES (4,18);
+INSERT INTO Recorrido_Calle(refRecorrido,refCalle) VALUES (4,19);
+INSERT INTO Recorrido_Calle(refRecorrido,refCalle) VALUES (4,20);
+INSERT INTO Recorrido_Calle(refRecorrido,refCalle) VALUES (4,59);
+INSERT INTO Recorrido_Calle(refRecorrido,refCalle) VALUES (4,57);
+INSERT INTO Recorrido_Calle(refRecorrido,refCalle) VALUES (4,56);
+INSERT INTO Recorrido_Calle(refRecorrido,refCalle) VALUES (4,54);
+INSERT INTO Recorrido_Calle(refRecorrido,refCalle) VALUES (4,53);
+INSERT INTO Recorrido_Calle(refRecorrido,refCalle) VALUES (4,28);
+INSERT INTO Recorrido_Calle(refRecorrido,refCalle) VALUES (4,84);
+INSERT INTO Recorrido_Calle(refRecorrido,refCalle) VALUES (4,85);
+INSERT INTO Recorrido_Calle(refRecorrido,refCalle) VALUES (4,31);
+INSERT INTO Recorrido_Calle(refRecorrido,refCalle) VALUES (4,32);
+INSERT INTO Recorrido_Calle(refRecorrido,refCalle) VALUES (4,33);
